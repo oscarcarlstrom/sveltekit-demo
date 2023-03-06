1454a1e9fae7785e7aa0901707e6698029956298
Oscar <oscar.carlstrom@bouvet.no>
2023-03-06 10:02:32 +0100

    Styling

--- src/lib/components/input.svelte
+++ src/lib/components/input.svelte
@@ -1,22 +1,22 @@
 <script lang="ts">
-  export let value = "";
-  export let placeholderText = "";
+	export let value = '';
+	export let placeholderText = '';
 </script>
 
 <input bind:value placeholder={placeholderText} />
 
 <style>
-  input {
-    font-size: 1.2rem;
-    padding: var(--spacing-m);
-    margin: 0 0 var(--spacing-m);
-    border-radius: var(--border-radius-m);
-    border: var(--border-style) var(--color-primary-light);
-    color: var(--color-primary);
-  }
+	input {
+		font-size: 1.2rem;
+		padding: var(--spacing-l);
+		margin: 0 0 var(--spacing-m);
+		border-radius: var(--border-radius-m);
+		border: var(--border-style) var(--color-primary-light);
+		color: var(--color-primary);
+	}
 
-  input:focus {
-    border-color: var(--color-primary);
-    outline: none;
-  }
+	input:focus {
+		border-color: var(--color-primary);
+		outline: none;
+	}
 </style>
