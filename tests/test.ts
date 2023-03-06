bdec1a228b677e565708236103acb446e825a327
Oscar <oscar.carlstrom@bouvet.no>
2023-03-06 13:00:46 +0100

    Added tests

--- tests/test.ts
+++ tests/test.ts
@@ -2,5 +2,11 @@ import { expect, test } from '@playwright/test';
 
 test('index page has expected h1', async ({ page }) => {
 	await page.goto('/');
-	await expect(page.getByRole('heading', { name: 'Welcome to SvelteKit' })).toBeVisible();
+	await expect(page.getByRole('heading', { name: 'My Svelte App', level: 1 })).toBeVisible();
+});
+
+test('login page has expected h1 and h2', async ({ page }) => {
+	await page.goto('/login');
+	await expect(page.getByRole('heading', { name: 'My Svelte App', level: 1 })).toBeVisible();
+	await expect(page.getByRole('heading', { name: 'Request a new token', level: 2 })).toBeVisible();
 });
